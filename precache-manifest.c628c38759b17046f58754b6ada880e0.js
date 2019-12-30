330c220aaecc8855e503702730218f111d06d2f3
dargap3 <dargap@hotmail.com>
2019-12-30 00:01:47 -0500

    Updates

---
+++ precache-manifest.c628c38759b17046f58754b6ada880e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdec42f9e8e8ea40450011a36916ee9d",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "705c59d05d1b37ff6e89",
+    "url": "/robots-rolodex/static/css/main.7aa0a2cd.chunk.css"
+  },
+  {
+    "revision": "39a2bd0350d99700a34c",
+    "url": "/robots-rolodex/static/js/2.7f4a0372.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robots-rolodex/static/js/2.7f4a0372.chunk.js.LICENSE"
+  },
+  {
+    "revision": "705c59d05d1b37ff6e89",
+    "url": "/robots-rolodex/static/js/main.6abb5fa3.chunk.js"
+  },
+  {
+    "revision": "e10dd8668fc04e7d61fa",
+    "url": "/robots-rolodex/static/js/runtime-main.24f364d3.js"
+  }
+]);
